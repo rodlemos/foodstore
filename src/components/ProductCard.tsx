35f682532fe460494076d757653d214d8974d76e
Rodrigo Lemos <rodrigosllemos@gmail.com>
2022-11-03 17:44:20 -0300

    product card space adjustments

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -11,7 +11,7 @@ export function ProductCard({ image, price, title }: Props) {
     <div className="max-w-xs mx-auto py-6 border-b border-b-darkPink/40">
       <img src={image} alt="" className="w-full object-cover aspect-video" />
 
-      <footer className="flex justify-between mt-6 items-center">
+      <footer className="flex justify-between mt-6 gap-1 items-start">
         <div>
           <h2 className="text-darkYellow">{title}</h2>
           <span className="text-darkPink font-bold text-2xl block">
